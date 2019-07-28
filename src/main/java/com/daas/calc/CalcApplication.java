c17c9647d702ceaab7aa187139087b0d91c959ad
mypractice96 <53252202+mypractice96@users.noreply.github.com>
2019-07-28 19:13:05 +0530

    Update CalcApplication.java

--- src/main/java/com/daas/calc/CalcApplication.java
+++ src/main/java/com/daas/calc/CalcApplication.java
@@ -10,7 +10,7 @@ public class CalcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalcApplication.class, args);
-		System.out.println("changed 2");
+		System.out.println("changed");
 		
 	}
 
