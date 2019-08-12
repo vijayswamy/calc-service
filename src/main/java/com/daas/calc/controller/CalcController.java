6e64ee9c589ca0c2552fefe147fa309c20e2849a
mypractice96 <53252202+mypractice96@users.noreply.github.com>
2019-08-12 22:55:40 +0530

    Update CalcController.java

--- src/main/java/com/daas/calc/controller/CalcController.java
+++ src/main/java/com/daas/calc/controller/CalcController.java
@@ -18,7 +18,7 @@ public class CalcController {
 
     @GetMapping(path="/test")
     public String test(){
-        return "Hello world!";
+        return "Hello world! Changing here";
     }
     
     @GetMapping(path="/add/{n1}/{n2}")
