80e75d6f4dd0e3c849cffe85ace9b2d0258f5288
mypractice96 <53252202+mypractice96@users.noreply.github.com>
2019-08-13 16:41:25 +0530

    Update CalcControllerTest.java

--- src/test/java/com/daas/calc/controller/CalcControllerTest.java
+++ src/test/java/com/daas/calc/controller/CalcControllerTest.java
@@ -1,1 +1,40 @@
-write code here
+package com.daas.calc.controller;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalcControllerTest {
+    CalcController calcController = new CalcController();
+
+   @Test
+    public void testApp() {
+        String str = calcController.test();
+        assertEquals(str instanceof String,str );
+    }  
+  
+ /* @Test
+    public void testAddition() {
+        int sum = calcService.add(2,3);
+        assertEquals(5,sum );
+
+    }
+    @Test
+    public void testDifference() {
+        int diff = calcService.difference(15,10);
+        assertEquals(5,diff );
+    }
+    @Test
+    public void testMultiply() {
+        int prod = calcService.multiply(2,3);
+        assertEquals(6,prod );
+    }
+
+    @Test
+    public void testDivision() {
+        int quot = calcService.divide(15,3);
+        assertEquals(5,quot );
+
+    }*/
+
+
+}
